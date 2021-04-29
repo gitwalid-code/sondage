67536027e47dd57eb47a846ba13858620fc1432b
Akram Moussaoui <akrammoussaoui14@gmail.com>
2021-04-29 11:40:09 +0100

    initiate express server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 2000;
+
+mongoose.connect(process.env.MONGO_DB, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => console.log("DataBase connected !!"));
+
+app.use(express.json());
+app.use(morgan("combined"));
+app.use(cors());
+app.use(helmet());
+
+app.listen(PORT, () => console.log("Listening on http://localhost" + PORT));
