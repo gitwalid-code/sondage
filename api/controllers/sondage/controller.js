7f79d4b08d6ce950a40d9f98a6633e10cbe46c07
Akram Moussaoui <akrammoussaoui14@gmail.com>
2021-05-03 15:12:39 +0100

    add sondage crud

---
+++ api/controllers/sondage/controller.js
@@ -0,0 +1,31 @@
+const Sondage = require("../../../models/sondage");
+
+const getSondages = async (req, res) => {};
+const getOneSondage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const sondage = await (await Sondage.findById(id)).populate("questions");
+    if (!sondage) {
+      return res.status(400).send({
+        message: "Sondage doesn't exist",
+      });
+    }
+    return res.status(200).send(sondage);
+  } catch (error) {
+    console.log("Error in getOneSondage =>", error);
+    return res.status(00).send({
+      message: "Server Error",
+    });
+  }
+};
+const addSondage = async (req, res) => {};
+const updateSondage = async (req, res) => {};
+const deleteSondage = async (req, res) => {};
+
+module.exports = {
+  getOneSondage,
+  getSondages,
+  addSondage,
+  updateSondage,
+  deleteSondage,
+};
